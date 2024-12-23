3a2b153bf80f6e4e922e9ae5831d626dc5fcd44f
bora <dianabora@naver.com>
2024-12-23 18:02:06 +0900

    search 1일차 - 검색기능 연습, list까지 구현

---
+++ src/main/java/com/woori/search/Constant/StoreConstant.java
@@ -0,0 +1,4 @@
+package com.woori.search.Constant;
+
+public class StoreConstant {
+}
---
+++ src/main/java/com/woori/search/DTO/StoreDTO.java
@@ -0,0 +1,27 @@
+//9. Entity를 참고해서 사용할 변수로 구성
+package com.woori.search.DTO;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@ToString @Builder
+public class StoreDTO {
+
+    private Integer idx;
+    private String storeName;
+    private String storeId;
+    private String storeChiefId;
+    private String storeChief;
+    private String storeTel;
+    private String storeDel;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
+
+    //HTML에서 추가로 받을 변수를 DTO로 함께 받고자 하면 변수를 추가
+    //Controller에서 개별변수로 받아서 사용
+    //private String search;
+}
+//DTO까지 설계가 완료되면 1차 확인(단위테스트)
